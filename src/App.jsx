f95833877d34ac91823cd72898fb02b3ebe9017e
TJ Klimoski <tjklimoski@gmail.com>
2023-05-09 19:19:34 -0400

    Establish base html and css

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,20 @@ function App() {
   
 
   return (
-    <h1>Hello</h1>
-  )
+    <>
+      <h1 className="title">Minesweeper</h1>
+      <div className="score">Mines Left: 10</div>
+      <div className="board">
+        <div className="tile" data-status="hide"></div>
+        <div className="tile" data-status="mine"></div>
+        <div className="tile" data-status="flag"></div>
+        <div className="tile" data-status="show">1</div>
+        <div className="tile" data-status="show">2</div>
+        <div className="tile" data-status="show">3</div>
+        <div className="tile" data-status="show"></div>
+      </div>
+    </>
+  );
 }
 
 export default App
