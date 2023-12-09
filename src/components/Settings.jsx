89e9e4db2b172321fc1c0dc23536ca5674e818a4
TJ Klimoski <tjklimoski@gmail.com>
2023-12-09 13:05:43 -0500

    Layout HTML for Settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,7 +1,32 @@
 import PropTypes from "prop-types";
 
 export default function Settings({ resetBoard }) {
-  return <div>Settings</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="bombCount">Number of bombs:</label>
+        <input
+          type="number"
+          id="bombCount"
+          name="bombCount"
+          min="2"
+          max="99"
+          step="1"
+          list="defaultBombCounts"
+          required
+        />
+        <datalist id="defaultBombCounts">
+          <option value="2" />
+          <option value="5" />
+          <option value="10" />
+          <option value="20" />
+          <option value="50" />
+          <option value="99" />
+        </datalist>
+      </div>
+      <button type="submit">Reset</button>
+    </form>
+  );
 }
 
 Settings.propTypes = {
