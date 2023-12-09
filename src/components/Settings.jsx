b1c989a81838d84f32ddaa29be6e080a8c75a21d
TJ Klimoski <tjklimoski@gmail.com>
2023-12-09 12:44:06 -0500

    Set Settings prop types

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types";
+
 export default function Settings({ resetBoard }) {
   return <div>Settings</div>;
 }
+
+Settings.propTypes = {
+  resetBoard: PropTypes.func,
+};
